02b831f600a51ed6ab1af493711227a3716a4637
Thomas J. Kennedy <tkennedy@cs.odu.edu>
2024-12-18 19:43:32 -0500

    Update CS 263 to s25

---
+++ cs263/latest/Assts/semester.pl
@@ -0,0 +1,23 @@
+#
+# Due dates for parts 1, 2, & 3
+$dueDate = "12/10/2016 00:05:00";
+#
+#
+# Where are the assignments located?
+#
+$asstsBase="/home/tkennedy/Courses/Websites/cs263/sum24/Assts/";
+#
+#
+# Where are the submissions and grades located?
+#
+$submissionsBase="/home/tkennedy/Courses/AutoGrading/cs263/sum24";
+#
+#
+# Instructor email address
+#
+$returnAddress = 'tkennedy@cs.odu.edu';
+#
+# Late actions
+#
+$lateAction = "Z";
+
